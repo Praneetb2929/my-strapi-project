5de9fd2ecc541b216f49de7068cb8097d0560e1e
Praneet Biswal <144131975+Praneetb2929@users.noreply.github.com>
2025-07-23 19:23:24 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,7 +1,32 @@
+# ECS Cluster
 resource "aws_ecs_cluster" "strapi_cluster" {
   name = "strapi-cluster"
 }
 
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ECS Task Definition
 resource "aws_ecs_task_definition" "strapi_task" {
   family                   = "strapi-task"
   network_mode            = "awsvpc"
@@ -9,41 +34,41 @@ resource "aws_ecs_task_definition" "strapi_task" {
   cpu                     = "512"
   memory                  = "1024"
   execution_role_arn      = aws_iam_role.ecs_task_execution_role.arn
-  container_definitions   = jsonencode([
-    {
-      name      = "strapi"
-      image     = var.docker_image_url
-      essential = true
-      portMappings = [
-        {
-          containerPort = 1337
-          protocol      = "tcp"
-        }
-      ]
-      environment = [
-        {
-          name  = "DATABASE_CLIENT"
-          value = "sqlite" # or postgres
-        }
-      ]
-    }
-  ])
+  container_definitions   = jsonencode([{
+    name      = "strapi"
+    image     = var.docker_image_url
+    essential = true
+    portMappings = [{
+      containerPort = 1337
+      protocol      = "tcp"
+    }]
+    environment = [{
+      name  = "DATABASE_CLIENT"
+      value = "sqlite" # or postgres
+    }]
+  }])
 }
 
+# ECS Service
 resource "aws_ecs_service" "strapi_service" {
   name            = "strapi-service"
   cluster         = aws_ecs_cluster.strapi_cluster.id
   task_definition = aws_ecs_task_definition.strapi_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
+
   network_configuration {
     subnets         = var.subnet_ids
     assign_public_ip = true
     security_groups  = [var.security_group_id]
   }
+
   load_balancer {
     target_group_arn = aws_lb_target_group.strapi_tg.arn
     container_name   = "strapi"
     container_port   = 1337
   }
+
+  depends_on = [aws_lb_target_group.strapi_tg]
 }
+
