bee45ec3a405546fee5f0af9940c777e522a833c
Praneet Biswal <144131975+Praneetb2929@users.noreply.github.com>
2025-07-23 20:03:44 +0530

    updations

---
+++ terraform/main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-strapi-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "strapi_ec2" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = aws_key_pair.deployer.key_name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install docker.io -y
+              sudo systemctl start docker
+              sudo docker pull ${var.docker_image_url}
+              sudo docker run -d -p 1337:1337 ${var.docker_image_url}
+              EOF
+
+  tags = {
+    Name = "strapi-ec2"
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,22 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "ami_id" {
+  default = "ami-0f5ee92e2d63afc18" # Ubuntu 22.04 (update as per region)
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
 variable "docker_image_url" {
-  description = "Docker image URL for the Strapi container"
-  type        = string
+  description = "Docker image to pull and run"
 }
 
-variable "subnet_ids" {
-  description = "List of subnet IDs for ECS tasks"
-  type        = list(string)
-}
-
-variable "security_group_id" {
-  description = "Security group ID attached to the ECS tasks"
-  type        = string
-}
+variable "subnet_id" {}
+variable "vpc_id" {}
+variable "public_key_path" {}
