f84117e1cac057303fe8675d6980ebfccbded765
Praneet Biswal <144131975+Praneetb2929@users.noreply.github.com>
2025-07-23 19:28:03 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,13 @@ variable "docker_image_url" {
   description = "Docker image URL for the Strapi container"
   type        = string
 }
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for ECS tasks"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "Security group ID attached to the ECS tasks"
+  type        = string
+}
